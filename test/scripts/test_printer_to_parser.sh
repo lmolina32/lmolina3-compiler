c6d47c774e9390049e2a71af8d8302b36e701bf0
Leonardo Molina <lmolina3@nd.edu>
2025-10-29 11:50:52 -0400

    chore: update docs and test

--- test/scripts/test_printer_to_parser.sh
+++ test/scripts/test_printer_to_parser.sh
@@ -10,10 +10,19 @@ echo "Pretty print files pass to parser"
 for testfile in ./test/printer/good*.bminor
 do
     ./bin/bminor --print $testfile &> $testfile.printer.out
-	if ./bin/bminor --parse $testfile.printer.out &> $testfile.printer_to_parse.out
+	if ./bin/bminor --parse $testfile.printer.out &> /dev/null
 	then
 		echo -e "$testfile.printer.out ${GREEN}success${NC} (as expected)"
 	else
 		echo -e "$testfile.printer.out ${RED}failure${NC} (INCORRECT)"
 	fi
+
+    ./bin/bminor --print $testfile.printer.out &> $testfile.printer.printer.out
+	if cmp -s "$testfile.printer.out" "$testfile.printer.printer.out"; 
+	then 
+		echo -e "Printer outputs are the same ${GREEN}success${NC} (as expected)"
+	else 
+		echo -e "Printer outputs are not the same ${RED}failure${NC} (INCORRECT)"
+	fi
+
 done
