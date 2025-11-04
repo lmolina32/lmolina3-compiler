38f785c75a263459cd32d11d54a3bb91dc943141
Leonardo Molina <lmolina3@nd.edu>
2025-11-04 18:48:58 -0500

    feat: create symbol table and scope functions

---
+++ src/symbol_table/scope.c
@@ -0,0 +1,105 @@
+/* scope.c: scope function definitions */
+
+#include "hash_table.h"
+#include "scope.h"
+#include "symbol.h"
+#include "utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* global variable */ 
+
+Symbol_stack stack = {
+    .top = 0,
+    .size = 0,
+    .status = 0,
+};
+
+/* functions */
+
+/**
+ * This functions adds a new scope (hash table) to the stack and updates metrics 
+ **/
+void scope_enter(){
+    struct hash_table *h = hash_table_create(0, 0);
+    stack.size += 1;
+    Symbol_node *curr = stack.top;
+    Symbol_node *node = safe_calloc(sizeof(Symbol_node), 1);
+    node->hashmap = h;
+    node->next = curr;
+    if (stack.size > 1) {
+        node->local = curr->local;
+    }
+    stack.top = node;
+}
+
+/**
+ * This function deletes the top most scope (hash table) from the stack
+ **/
+void scope_exit(){
+    Symbol_node *curr = stack.top;
+    stack.top = curr->next;
+    stack.size -= 1;
+    hash_table_delete(curr->hashmap);
+    free(curr);
+}
+
+/**
+ * This function returns how many scopes are currently in the stack 
+ * @return      the number of scopes in the stack
+ **/
+int scope_level(){
+    return stack.size;
+}
+
+/**
+ * Adds entry to topmost hash table in the stack, binds name with symbol
+ * @param   name        The identifier to bind symbol structure 
+ * @param   sym         The symbols structure describing the identifier
+ **/
+void scope_bind( const char *name, Symbol *sym ){
+    if (!name || !sym || !stack.top) return;
+
+    if (sym->kind == SYMBOL_PARAM){
+        sym->which = stack.top->params;
+        stack.top->params++;
+    } else {
+        sym->which = stack.top->local;
+        stack.top->local++;
+    }
+
+    if (!hash_table_insert(stack.top->hashmap, name, (void *)sym)){
+        fprintf(stderr, "scope_bind: hash table insert failed for %s", name);
+        exit(1);
+    }
+}
+
+/**
+ * Searches the stack of hash tables from top to bottom, looking for first entry that matches name extactly. 
+ * @param   name        Identifier we are trying to find 
+ * @return  Struct symbol corresponding to identifier, NULL if not found 
+ **/
+Symbol *scope_lookup( const char *name ){
+    if (!name) return NULL;
+
+    Symbol_node *curr = stack.top;
+    while (curr){
+        Symbol *sym = (Symbol *)hash_table_lookup(curr->hashmap, name);
+        if (sym) return sym;
+        curr = curr->next;
+    }
+
+    return NULL;
+}
+
+/**
+ * Searches top hash table for first enry that matches name exactly 
+ * @param   name        Identifier to search in hash table
+ * @return  Struct symbol corresponding to identifier, NULL if not found
+ **/
+Symbol *scope_lookup_current( const char *name ){
+    if (!name || !stack.top) return NULL;
+    return (Symbol *)hash_table_lookup(stack.top->hashmap, name);
+}
+
---
+++ src/symbol_table/scope.h
@@ -0,0 +1,44 @@
+/* scop .h: scope structure and functions */
+
+#ifndef SCOPE_H
+#define SCOPE_H
+
+#include <stdio.h>
+
+/* forward declaration */
+
+typedef struct Symbol Symbol;
+
+/* Strcutres */ 
+
+typedef struct Symbol_node Symbol_node;
+
+struct Symbol_node {
+    struct hash_table *hashmap;
+    Symbol_node *next; 
+    int params;
+    int local;
+};
+
+typedef struct Symbol_stack Symbol_stack;
+
+struct Symbol_stack {
+    Symbol_node *top;
+    int size; 
+    int status; 
+};
+
+/* Global Variables */
+
+extern Symbol_stack stack;
+
+/* function s*/
+
+void    scope_enter();
+void    scope_exit();
+int     scope_level();
+void    scope_bind( const char *name, Symbol *sym );
+Symbol *scope_lookup( const char *name );
+Symbol *scope_lookup_current( const char *name );
+
+#endif
