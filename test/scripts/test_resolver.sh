cfb28eb090d20d6d3827192caae7f01c92ff130f
Leonardo Molina <lmolina3@nd.edu>
2025-11-04 18:49:42 -0500

    test: create resolver test script + test cases

---
+++ test/scripts/test_resolver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# run resolver tests
+
+GREEN='\e[32m'
+RED='\e[31m'
+NC='\e[0m'
+
+for testfile in ./test/resolver/good*.bminor
+do
+	if ./bin/bminor --resolve $testfile &> $testfile.out
+	then
+		echo -e "$testfile ${GREEN}success${NC} (as expected)"
+	else
+		echo -e "$testfile ${RED}failure${NC} (INCORRECT)"
+	fi
+done
