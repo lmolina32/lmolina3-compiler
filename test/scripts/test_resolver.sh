57b183c13c117073da45103166e8204058a04ee4
Leonardo Molina <lmolina3@nd.edu>
2025-11-04 20:23:03 -0500

    test: create test cases

--- test/scripts/test_resolver.sh
+++ test/scripts/test_resolver.sh
@@ -15,3 +15,13 @@ do
 		echo -e "$testfile ${RED}failure${NC} (INCORRECT)"
 	fi
 done
+
+for testfile in ./test/resolver/bad*.bminor
+do
+	if ./bin/bminor --resolve $testfile &> $testfile.out
+	then
+		echo -e "$testfile ${GREEN}success${NC} (INCORRECT)"
+	else
+		echo -e "$testfile ${RED}failure${NC} (as expected)"
+	fi
+done
